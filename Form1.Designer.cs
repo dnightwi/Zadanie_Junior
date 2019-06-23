6568a664a7facce6bb14fe25a2f28db40f7b099d
dnightwi <50114930+dnightwi@users.noreply.github.com>
2019-06-24 00:39:43 +0300

    Project finished

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Zadanie_Junior
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(12, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Применить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button2_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(415, 17);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(197, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(127, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(415, 56);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(147, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.TextChanged += new System.EventHandler(this.TextBox5_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "И",
+            "ИЛИ"});
+            this.comboBox1.Location = new System.Drawing.Point(126, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(53, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "И",
+            "ИЛИ"});
+            this.comboBox2.Location = new System.Drawing.Point(342, 16);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(53, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "И",
+            "ИЛИ"});
+            this.comboBox4.Location = new System.Drawing.Point(342, 54);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(53, 21);
+            this.comboBox4.TabIndex = 9;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.ComboBox4_SelectedIndexChanged_1);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "И",
+            "ИЛИ"});
+            this.comboBox3.Location = new System.Drawing.Point(126, 51);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.comboBox3.Size = new System.Drawing.Size(53, 21);
+            this.comboBox3.TabIndex = 10;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.ComboBox4_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 119);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(687, 211);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(711, 342);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Zadanie_Junior";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_Junior
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(openFile.FileName);
+            }
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ComboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ComboBox4_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            string[] fullTxt;
+
+            fullTxt = richTextBox1.Text.Split('\n');
+            richTextBox1.Text = "";
+            foreach (string search in fullTxt)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        if (comboBox3.SelectedIndex == 0)
+                        {
+                            if (comboBox4.SelectedIndex == 0)
+                            {
+                                if (search.Contains(textBox1.Text) && search.Contains(textBox2.Text) &&
+                                    search.Contains(textBox3.Text) && search.Contains(textBox4.Text))
+                                    richTextBox1.Text += search + "\n";
+                            }
+                            else
+                            {
+                                if (search.Contains(textBox1.Text) && search.Contains(textBox2.Text) &&
+                                    search.Contains(textBox3.Text))
+                                    richTextBox1.Text += search + "\n";
+                            }
+                        }
+                        else if (comboBox4.SelectedIndex == 0)
+                        {
+                            if (search.Contains(textBox1.Text) && search.Contains(textBox2.Text) &&
+                                search.Contains(textBox4.Text))
+                                richTextBox1.Text += search + "\n";
+                        }
+                        else
+                        {
+                            if (search.Contains(textBox1.Text) && search.Contains(textBox2.Text))
+                                richTextBox1.Text += search + "\n";
+                        }
+                    }
+                    else if (comboBox3.SelectedIndex == 0)
+                    {
+                        if (comboBox4.SelectedIndex == 0)
+                        {
+                            if (search.Contains(textBox1.Text) && search.Contains(textBox3.Text) &&
+                                search.Contains(textBox4.Text))
+                                richTextBox1.Text += search + "\n";
+                        }
+                        else
+                        {
+                            if (search.Contains(textBox1.Text) && search.Contains(textBox3.Text))
+                                richTextBox1.Text += search + "\n";
+                        }
+                    }
+                    else if (comboBox4.SelectedIndex == 0)
+                    {
+                        if (search.Contains(textBox1.Text) && search.Contains(textBox4.Text))
+                            richTextBox1.Text += search + "\n";
+                    }
+                    else
+                    {
+                        if (search.Contains(textBox1.Text))
+                            richTextBox1.Text += search + "\n";
+                    }
+                }
+                else if (comboBox2.SelectedIndex == 0)
+                {
+                    if (comboBox3.SelectedIndex == 0)
+                    {
+                        if (comboBox4.SelectedIndex == 0)
+                        {
+                            if (search.Contains(textBox2.Text) && search.Contains(textBox3.Text) &&
+                                search.Contains(textBox4.Text))
+                                richTextBox1.Text += search + "\n";
+                        }
+                        else
+                        {
+                            if (search.Contains(textBox2.Text) && search.Contains(textBox3.Text))
+                                richTextBox1.Text += search + "\n";
+                        }
+                    }
+                    else if (comboBox4.SelectedIndex == 0)
+                    {
+                        if (search.Contains(textBox2.Text) && search.Contains(textBox4.Text))
+                            richTextBox1.Text += search + "\n";
+                    }
+                    else
+                    {
+                        if (search.Contains(textBox2.Text))
+                            richTextBox1.Text += search + "\n";
+                    }
+                }
+                else if (comboBox3.SelectedIndex == 0)
+                {
+                    if (comboBox4.SelectedIndex == 0)
+                    {
+                        if (search.Contains(textBox3.Text) && search.Contains(textBox4.Text))
+                            richTextBox1.Text += search + "\n";
+                    }
+                    else
+                    {
+                        if (search.Contains(textBox3.Text))
+                            richTextBox1.Text += search + "\n";
+                    }
+                }
+                else if (comboBox4.SelectedIndex == 0)
+                {
+                    if (search.Contains(textBox4.Text))
+                        richTextBox1.Text += search + "\n";
+                }
+                else if (search.Contains(textBox1.Text) || search.Contains(textBox2.Text) ||
+                       search.Contains(textBox3.Text) || search.Contains(textBox4.Text))
+                    richTextBox1.Text += search + "\n";
+            }
+        }
+
+    }
+}
